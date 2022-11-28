368e27acf14a603bda4b20a97bb10e4d8dcacfa4
cheol <qjel60@naver.com>
2022-11-28 19:17:25 +0900

    head 브랜드 더보기란 구현만 완료

---
+++ public/js/index.js
@@ -0,0 +1,12 @@
+const head_topmenu = document.querySelector(".head_topmenu")
+const head_any_button = document.querySelector("#head_any_button")
+
+function head_topmenuHandler(){
+    if (head_any_button.className === "on"){
+        head_any_button.className = "off"
+    } else {
+        head_any_button.className = "on"
+    }
+}
+
+head_topmenu.addEventListener("click", head_topmenuHandler)
