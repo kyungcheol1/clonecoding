c378f85da8e96e5ae03397f9ae0b5a87535b6f18
cheol <qjel60@naver.com>
2022-11-29 14:21:34 +0900

    head:popup 부분 slide, 검색어 순위 구현

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,11 @@
 const head_topmenu = document.querySelector(".head_topmenu")
 const head_any_button = document.querySelector("#head_any_button")
+const searchinput = document.querySelector("#searchinput")
+const search_popup = document.querySelector("#search_popup")
+const popuppre = document.querySelector(".popuppre")
+const popupnext = document.querySelector(".popupnext")
+const product = document.querySelector("#product")
+const pag_number = document.querySelector(".pag_number")
 
 function head_topmenuHandler(e){
     const target = e.target
@@ -10,4 +16,26 @@ function head_topmenuHandler(e){
     }
 }
 
+function searchinputHandler(e){
+    const target = e.target
+    if (search_popup.className === "on") {
+        search_popup.className = "off"
+    } else {
+        search_popup.className = "on"
+    }    
+}
+
+function popuppreHandler(e){
+    product.className = "page1"
+    pag_number.innerHTML = "01-02"
+}
+
+function popupnextHandler(e){
+    product.className = "page2"
+    pag_number.innerHTML = "02-02"
+}
+
 head_topmenu.addEventListener("click", head_topmenuHandler)
+searchinput.addEventListener("click", searchinputHandler)
+popuppre.addEventListener("click", popuppreHandler)
+popupnext.addEventListener("click", popupnextHandler)
